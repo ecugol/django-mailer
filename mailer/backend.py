0429d55ed170dd10675bae52ffb632b8e8c93d50
Ivan Vedernikov <ecugol@gmail.com>
2015-01-30 03:21:07 +0500

    Adds an ability to set email subject prefix via settings.MAILER_EMAIL_SUBJECT_PREFIX

--- mailer/backend.py
+++ mailer/backend.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.mail.backends.base import BaseEmailBackend
 
 from mailer.models import Message
@@ -7,7 +8,10 @@ class DbBackend(BaseEmailBackend):
 
     def send_messages(self, email_messages):
         num_sent = 0
+        subject_prefix = getattr(settings, 'MAILER_EMAIL_SUBJECT_PREFIX', None)
         for email in email_messages:
+            if subject_prefix:
+                email.subject = u'%s %s' % (subject_prefix, email.subject)
             msg = Message()
             msg.email = email
             msg.save()
